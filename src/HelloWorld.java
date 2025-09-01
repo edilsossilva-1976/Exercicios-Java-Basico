50721567513cacf46e2074b51b77583163febf35
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-01 16:35:08 -0300

    Exercício Olá mundo

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+ * Escreva um programa Java para imprimir "Olá" na tela e seu nome em uma linha separada.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá ");
+        System.out.println("Edilson!");
+    }
+}
