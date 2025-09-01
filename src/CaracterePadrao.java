83fe80146aecd7d55319c6775956089c1c13ac5f
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-01 17:58:08 -0300

    Utilizando caracteres para desenhar a palavra java

---
+++ src/CaracterePadrao.java
@@ -0,0 +1,16 @@
+/*
+ * 8.	Escreva um programa Java para exibir o seguinte padrão.
+ *     J    a   v     v  a                                                  
+ *     J   a a   v   v  a a                                                 
+ *  J  J  aaaaa   V V  aaaaa                                                
+ *   JJ  a     a   V  a     a
+ * 
+ */
+public class CaracterePadrao {
+    public static void main(String[] args) {
+        System.out.println("    J    a   v      v  a");
+        System.out.println("    J   a  a  v    v  a a");
+        System.out.println("J   J  aaaaa   v  v  aaaaaa");
+        System.out.println("  JJ  a     a    v  a      a");
+    }
+}
