f957bef164e8d456fc2e3232fe214de89b563fe2
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-01 16:54:20 -0300

    Exercicio para imprimir resultados propostos

---
+++ src/ImprimirResultados.java
@@ -0,0 +1,16 @@
+/*
+ * Escreva um programa Java para imprimir os resultados das seguintes operações abaixo.
+ * a.	-5 + 8 * 6
+ * b.	(55 + 9) % 9
+ * c.	20 + -3 * 5 / 8
+ * d.	5 + 15 / 3 * 2 – 8 % 3
+ */
+public class ImprimirResultados {
+    public static void main(String[] args) {
+        System.out.println("-5 + 8 * 6 = " + (-5 + 8 * 6));
+        System.out.println("(55 + 9) % 9 = "+ ((55+9)%9));
+        System.out.println("20 + (-3 * 5 / 8) = "+ (20 + (-3 * 5 / 8)));
+        System.out.println("5 + 15 / 3 * 2 - 8 % 3 = "+ (5 + 15 / 3 * 2 - 8 % 3));
+
+    }
+}
