85322e09554c744bfcb6bc9994ed5073108fd79b
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-01 16:37:05 -0300

    Exercicio soma de 2 numeros

---
+++ src/Soma2Numeros.java
@@ -0,0 +1,8 @@
+/*
+ * Escreva um programa Java para imprimir a soma de dois números.
+ */
+public class Soma2Numeros {
+    public static void main(String[] args) {
+        System.out.println("2 + 3 = "+(2+3));
+    }
+}
