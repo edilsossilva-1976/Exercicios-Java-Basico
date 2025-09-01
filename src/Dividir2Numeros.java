c6dd544ea1a9b38a0f41365a8716a83aeb01da5b
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-01 16:39:10 -0300

    Exercicio que mostra na tela, divisão de 2 numeros

---
+++ src/Dividir2Numeros.java
@@ -0,0 +1,8 @@
+/*
+ * Escreva um programa Java para dividir dois números e imprimi-los na tela.
+ */
+public class Dividir2Numeros {
+    public static void main(String[] args) {
+        System.out.println("8 / 2 = "+ (8/2));
+    }
+}
