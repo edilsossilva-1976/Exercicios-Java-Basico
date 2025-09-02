8b1172c43b8a1fa2bead42864bb006121b5079a1
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-02 13:08:32 -0300

    Exercicio que desenha uma bandeira americana

---
+++ src/AmericanFlag.java
@@ -0,0 +1,22 @@
+/*
+ * Escreva um programa Java para imprimir uma bandeira americana na tela.
+ */
+public class AmericanFlag {
+    public static void main(String[] args) {
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
