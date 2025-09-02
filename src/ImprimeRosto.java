64672a19b3c4cb48bcbc9f4323565bef11cd1a82
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-02 13:36:58 -0300

    Exercicio que desenha um rosto

---
+++ src/ImprimeRosto.java
@@ -0,0 +1,13 @@
+/*
+ * Escreva um programa Java para imprimir um rosto.
+ */
+public class ImprimeRosto {
+    public static void main(String[] args) {
+
+        System.out.println("  +'''''+ ");
+        System.out.println(" [| 0 0 |] ");
+        System.out.println("  |  ^  | ");
+        System.out.println("  | '_' | ");
+        System.out.println("  +-----+ ");
+    }
+}
